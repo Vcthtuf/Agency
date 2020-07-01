e4eb1c0644ad646a139b01eed24d85aab1dcb67a
Melnikov Sergey <qazxswedc1357924680@whatever.com>
2020-07-01 23:05:29 +0500

    Make adaptive 1200px and 992px. Add photo for slider

--- build/js/script.js
+++ build/js/script.js
@@ -1,17 +1,27 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+let menu = $("#menu");
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+    $(window).scroll(function() {
+        var top = $(this).scrollTop();
+        if ( top >= 700 ) {
+        menu.addClass('header_nav_scroll'); 
+        } else if ( top <= 700 ) {
+        menu.removeClass('header_nav_scroll'); 
+        }
     });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+$(document).ready(function() { 
+    $("nav ul li a").click(function () { 
+        elementClick = $(this).attr("href"); 
+        destination = $(elementClick).offset().top; 
+        $("body,html").animate({scrollTop: destination }, 800); 
+    }); 
+});
+
+$(document).ready(function() { 
+    $("footer ul li a").click(function () { 
+        elementClick = $(this).attr("href"); 
+        destination = $(elementClick).offset().top; 
+        $("body,html").animate({scrollTop: destination }, 800); 
+    }); 
+});
+
