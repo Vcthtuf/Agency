83165865f8952e09db199e31d01fadaf9f35a3ac
Melnikov Sergey <qazxswedc1357924680@whatever.com>
2020-07-02 16:06:14 +0500

    End adaptive

--- build/js/script.js
+++ build/js/script.js
@@ -1,11 +1,16 @@
 let menu = $("#menu");
+let hamburger = $('#hamburger');
+let heightHeader = parseInt($('#home').css('height'))+ parseInt($('#home').css('padding-top'))+ parseInt($('#home').css('padding-bottom'));
+console.log(heightHeader);
 
     $(window).scroll(function() {
         var top = $(this).scrollTop();
-        if ( top >= 700 ) {
-        menu.addClass('header_nav_scroll'); 
-        } else if ( top <= 700 ) {
-        menu.removeClass('header_nav_scroll'); 
+        if ( top >= heightHeader ) {
+        menu.addClass('header_nav_scroll');
+        hamburger.addClass('hamburger_scroll');
+        } else if ( top <= heightHeader ) {
+        menu.removeClass('header_nav_scroll');
+        hamburger.removeClass('hamburger_scroll');
         }
     });
 
@@ -24,4 +29,22 @@ $(document).ready(function() {
         $("body,html").animate({scrollTop: destination }, 800); 
     }); 
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header_menu'),
+    menuItem = document.querySelectorAll('.header_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header_menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header_menu_active');
+        })
+    })
+})
 
