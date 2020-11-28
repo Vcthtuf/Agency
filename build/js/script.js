cfb05d8cedb4ca32b13956681b7ce5f1d9eb2f5c
Vcthtuf <Vcthtuf@gmail.com>
2020-11-28 18:44:45 +0500

    Added slider in header

--- build/js/script.js
+++ build/js/script.js
@@ -1,18 +1,17 @@
 let menu = $("#menu");
 let hamburger = $('#hamburger');
-let heightHeader = parseInt($('#home').css('height'))+ parseInt($('#home').css('padding-top'))+ parseInt($('#home').css('padding-bottom'));
-console.log(heightHeader);
+let heightHeader = parseInt($('#home').css('height')) + parseInt($('#home').css('padding-top')) + parseInt($('#home').css('padding-bottom'));
 
-    $(window).scroll(function() {
-        var top = $(this).scrollTop();
-        if ( top >= heightHeader ) {
+$(window).scroll(function () {
+    var top = $(this).scrollTop();
+    if (top >= heightHeader) {
         menu.addClass('header_nav_scroll');
         hamburger.addClass('hamburger_scroll');
-        } else if ( top <= heightHeader ) {
+    } else if (top <= heightHeader) {
         menu.removeClass('header_nav_scroll');
         hamburger.removeClass('hamburger_scroll');
-        }
-    });
+    }
+});
 
 /*(document).ready(function() { 
     $("nav ul li a").click(function () { 
@@ -32,8 +31,8 @@ $(document).ready(function() {
 
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.header_menu'),
-    menuItem = document.querySelectorAll('.header_item'),
-    hamburger = document.querySelector('.hamburger');
+        menuItem = document.querySelectorAll('.header_item'),
+        hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
@@ -46,5 +45,35 @@ window.addEventListener('DOMContentLoaded', () => {
             menu.classList.toggle('header_menu_active');
         })
     })
+
+    // slider 
+
+    let prev = document.querySelector('.arrow_left'),
+        next = document.querySelector('.arrow_right'),
+        position = document.querySelector('.slider__track'),
+        slides = document.querySelectorAll('.slide'),
+        indexSlide = 1;
+
+    function showSlide(n) {
+
+        if (n < 1) { indexSlide = slides.length; }
+        if (n > (slides.length)) { indexSlide = 1; }
+
+        position.style.right = (indexSlide - 1) * 100 + '%';
+
+    };
+
+    function plusSlides(n) {
+        showSlide(indexSlide += n);
+    };
+
+    prev.addEventListener('click', function () {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function () {
+        plusSlides(1);
+    });
+
 })
 
